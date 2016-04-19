c815c6aadcd9129fcf5ec48c0dcc543d39ac03d9
HongYi Huang <hongyi.huang@agoda.com>
2016-04-19 16:34:54 +0700

    Initial Commit

---
+++ passengerGuard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  passengerGuard
+//
+//  Created by Huang, Hong-Yi (Agoda) on 4/19/2559 BE.
+//  Copyright © 2559 Huang, Hong-Yi (Agoda). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
